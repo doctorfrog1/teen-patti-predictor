5e344d675a79cb201d27fa7de5207497b5fa4b16
doctorfrog1 <doctorgames2024@gmail.com>
2025-06-10 17:26:44 +0530

    Update casino_tracker.py

--- casino_tracker.py
+++ casino_tracker.py
@@ -176,7 +176,8 @@ else:
     st.write("ERROR: 'connections' section NOT found in st.secrets!")
 
 st.write("--- End Debugging st.secrets ---")
-    conn = st.connection("my_data")
+
+conn = st.connection("my_data")
     model = None
     le = None
 
